ccbec1895874feb2a5632b954ce0854257dfa483
Leandro Lago da Silva <leandro@datacampo.com.br>
2023-01-12 15:43:24 -0300

    turn admin user

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,10 +3,13 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = request.params.user_id;
+    const all = this.turnUserAdminUseCase.execute({ user_id });
+
+    return response.json(all);
   }
 }
 

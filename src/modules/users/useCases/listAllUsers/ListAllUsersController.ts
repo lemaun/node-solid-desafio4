6cd8d5e1ec12070333218526b581191c95450503
Leandro Lago da Silva <leandro@datacampo.com.br>
2023-01-12 15:25:23 -0300

    list all users

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
-
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+  constructor(private listAllUsersUseCase: ListAllUsersUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = request.params.user_id;
+    const all = this.listAllUsersUseCase.execute({ user_id });
+
+    return response.json(all);
   }
 }
 
